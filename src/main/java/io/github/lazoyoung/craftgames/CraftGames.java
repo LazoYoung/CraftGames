d9645774932c4a0a93fb1be993c83984828c16eb
LazoYoung <myblog11@naver.com>
2018-02-09 15:03:15 +0900

    Basic script functions.

--- src/main/java/io/github/lazoyoung/craftgames/CraftGames.java
+++ src/main/java/io/github/lazoyoung/craftgames/CraftGames.java
@@ -1,26 +1,59 @@
 package io.github.lazoyoung.craftgames;
 
 import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
 
 import java.nio.file.Path;
 
+@Plugin(id = "craftgames", name = "CraftGames", version = "0.1.0", authors = "LazoYoung")
 public class CraftGames {
     
     @Inject
+    @ConfigDir(sharedRoot = false)
     private Path configDir;
+    
+    @Inject
+    private Logger log;
+    
+    private Game game;
     
     private static ObjectHolder holder;
     
     
+    /**
+     * @return the plugin instance. This will return null until plugin gets initialized.
+     */
     public static CraftGames getInstance() {
         return holder.obj;
+    }
+    
+    public Logger getLogger() {
+        return log;
+    }
+    
+    public Path getConfigDir() {
+        return configDir;
     }
     
     @Listener
     public void initPlugin(GamePreInitializationEvent event) {
         holder = new ObjectHolder(this);
+        game = new Game();
+        Sponge.getCommandManager().register(getInstance(), buildCommand(), "craftgames", "craftgame", "cg");
+    }
+    
+    private CommandSpec buildCommand() {
+        return CommandSpec.builder()
+                .description(Text.of("CraftGames Command"))
+                .child(game.scriptCommand.get(), "runscript", "testscript", "script")
+                .build();
     }
     
 }
--- src/main/java/io/github/lazoyoung/craftgames/Game.java
+++ src/main/java/io/github/lazoyoung/craftgames/Game.java
@@ -1,55 +1,150 @@
 package io.github.lazoyoung.craftgames;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.asset.Asset;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.event.EventListener;
-import org.spongepowered.api.event.EventManager;
+import org.spongepowered.api.text.Text;
+
+import javax.script.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class Game {
     
-    private Script script;
+    public ScriptCommand scriptCommand;
+    private CraftGames plugin;
+    private ScriptEngine engine;
+    private ScriptContext context;
+    private Bindings bindings;
+    private Path dir;
     
     public Game() {
-        script = new Script(Sponge.getEventManager());
+        plugin = CraftGames.getInstance();
+        dir = plugin.getConfigDir();
+        scriptCommand = new ScriptCommand(this);
+        engine = new ScriptEngineManager().getEngineByName("nashorn");
+        context = new SimpleScriptContext();
+        bindings = new SimpleBindings();
+    
+        try {
+            bindings.put("EventListener", engine.eval("Java.type('org.spongepowered.api.event.EventListener')"));
+            bindings.put("registerListener", (BiConsumer<Class, EventListener>) this::registerListener);
+            bindings.put("getEvent", (Function<String, Class>) this::getEvent);
+            context.setBindings(engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE), ScriptContext.ENGINE_SCOPE);
+            context.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
     }
     
-    public void start() {
-        // TODO Scripts must be able to access the non-private fields in this class
-        // TODO Evaluate the script
+    /**
+     *
+     * @param fileName including filename extension
+     * @param copyIfAbsent If file is absent, copy it from plugin.jar if available
+     * @return False if file is absent
+     * @throws ScriptException may occur if any syntax error exists
+     */
+    boolean runScript(String fileName, boolean copyIfAbsent) throws ScriptException {
+        Path path = dir.resolve(fileName);
+        File file = path.toFile();
+        dir.toFile().mkdirs();
         
+        try {
+            engine.eval(new FileReader(file), bindings);
+        }
+        
+        catch (FileNotFoundException e) {
+            if(copyIfAbsent) {
+                Optional<Asset> asset = Sponge.getAssetManager().getAsset(plugin, fileName);
+                
+                if(asset.isPresent()) {
+                    try {
+                        asset.get().copyToFile(path);
+                        return runScript(fileName, false);
+                    } catch (IOException | ScriptException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            
+            return false;
+        }
+        
+        return true;
     }
     
-    /**
-     * Register a event function in your game script.
-     * @param eventClass The type of event. Use Java.type(String classpath)
-     * @param listener The instance where you can implement your event function
-     */
-    void registerEvent(Class eventClass, EventListener listener) {
-        script.registerListener(eventClass, listener);
+    @SuppressWarnings("unchecked")
+    private void registerListener(Class eventClass, EventListener listener) {
+        Sponge.getEventManager().registerListener(CraftGames.getInstance(), eventClass, listener);
     }
     
-    /**
-     * Register a scheduler function in your game script.
-     */
-    void registerScheduler() {
-    
+    private Class getEvent(String eventPath) {
+        if(!eventPath.startsWith("org.")) {
+            eventPath = "org.spongepowered.api.event." + eventPath;
+        }
+        
+        try {
+            return Class.forName(eventPath);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
     }
     
 }
 
-class Script {
+class ScriptCommand implements CommandExecutor {
     
-    private EventManager man;
+    private Game game;
     
-    Script(EventManager man) throws IllegalArgumentException {
-        this.man = man;
+    public ScriptCommand(Game game) {
+        this.game = game;
+    }
+    
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) {
+        Optional<String> fileNameArg = args.getOne("fileName");
         
-        if(man == null) {
-            throw new IllegalArgumentException();
+        if(!fileNameArg.isPresent()) {
+            return CommandResult.empty();
         }
+        
+        String fileName = fileNameArg.get();
+        
+        try {
+            if(!game.runScript(fileName, true)) {
+                src.sendMessage(Text.of("Script file not found: " + fileName));
+            }
+            else {
+                src.sendMessage(Text.of("Executed the script successfully."));
+            }
+        } catch (ScriptException e) {
+            src.sendMessage(Text.of("Script error occurred. Exception stacktrace was printed in console."));
+            e.printStackTrace();
+        }
+        
+        return CommandResult.success();
     }
     
-    void registerListener(Class eventClass, EventListener listener) {
-        man.registerListener(CraftGames.getInstance(), eventClass, listener);
+    public CommandSpec get() {
+        return CommandSpec.builder()
+                .description(Text.of("Run a script with given <fileName> for test purpose."))
+                .permission("craftgames.runscript")
+                .arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("fileName"))))
+                .executor(this)
+                .build();
     }
-    
 }
