3281dcb01ba8083f12bbae918176cc991cf0465b
LazoYoung <idealproduction@naver.com>
2020-05-30 19:15:16 +0900

    Modify game start logic in order to fix #86

--- src/main/kotlin/com/github/lazoyoung/craftgames/api/event/GameStartEvent.kt
+++ src/main/kotlin/com/github/lazoyoung/craftgames/api/event/GameStartEvent.kt
@@ -3,6 +3,9 @@ package com.github.lazoyoung.craftgames.api.event
 import com.github.lazoyoung.craftgames.impl.game.Game
 import org.bukkit.event.HandlerList
 
+/**
+ * This event is triggered once every players have been teleported into field.
+ */
 class GameStartEvent(game: Game) : GameEvent(game) {
 
     companion object {
